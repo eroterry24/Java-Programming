8c09271a1ca596d9df41989f5be37e05921c4fbd
Ero Terry <eroterry24@gmail.com>
2023-11-01 00:45:35 +0100

    add new attribute

---
+++ IdeaProjects/java-programming/src/ConditionalStatement.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ConditionalStatement {
+
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+
+        System.out.println("Enter first number; ");
+        int i = number.nextInt();
+
+        System.out.println(" Enter second number; ");
+        int j = number.nextInt();
+
+        if (i > j){
+            System.out.println(i + " is the largest number");
+            }
+
+            else {
+                System.out.println(j + "is the smallest number");
+
+            }
+        }
+    }
+
