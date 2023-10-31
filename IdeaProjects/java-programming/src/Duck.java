b4a0587c2b5263e6747c591f755bc51eff960793
Ero Terry <eroterry24@gmail.com>
2023-10-31 20:46:24 +0100

    add new attribute

---
+++ IdeaProjects/java-programming/src/Duck.java
@@ -0,0 +1,25 @@
+public class Duck {
+
+    private String name;
+
+    private int age;
+
+    private String skinColor;
+
+    private boolean isFriendly;
+
+    private String breed;
+
+    public Duck (String name, int age,String breed){
+        this.name= name;
+        this.breed = breed;
+        this.age = age;
+
+
+
+
+
+
+
+    }
+}
