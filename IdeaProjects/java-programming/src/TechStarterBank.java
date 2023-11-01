8cfa05f9ec4b664bb536becfee2e791339110058
Ero Terry <eroterry24@gmail.com>
2023-11-01 15:38:35 +0100

    add new attribute

--- IdeaProjects/java-programming/src/TechStarterBank.java
+++ IdeaProjects/java-programming/src/TechStarterBank.java
@@ -1,4 +1,20 @@
 public class TechStarterBank {
 
+    private double balance = 12000.0 - 50;
 
+
+    public double sendMoney(int amountToSend) throws Exception {
+
+        if (amountToSend > balance) {
+            throw new Exception("Insufficient balance");
+
+        }
+        double total = balance - amountToSend;
+        System.out.println("Amount transfer successfully and your balance is" + total);
+        return total;
+
+
+    }
 }
+
+
