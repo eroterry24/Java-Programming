714a220bc0f37690b5120314bcca98cfacf4527f
Ero Terry <eroterry24@gmail.com>
2023-11-01 16:45:01 +0100

    add new attribute

---
+++ IdeaProjects/java-programming/src/ArrayMethod.java
@@ -0,0 +1,11 @@
+public class ArrayMethod {
+
+    public static void main(String[] args) {
+    String [] usernames ={"Ade", "Lola", "Toli", "Micheal"};
+    int [] numbers = {1, 2, 3, 4, 5, 6, 7, 5, 25, 78,89};
+        System.out.println(usernames[3]);
+        System.out.println(numbers[5]);
+
+    }
+
+}
