629b54cd1bd72356e0e00c60e94c187f88c3cb09
Ero Terry <eroterry24@gmail.com>
2023-11-03 20:38:40 +0100

    add more attribute

---
+++ IdeaProjects/java-programming/src/ForLoop.java
@@ -0,0 +1,20 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+//         Declare where the loop should start from
+//   Give it a range, where it should end
+//   increament from the starting to the end
+//   point
+
+//        first thing to do is to create a variable
+        for (int i = 0; i <=5; i++){
+            System.out.println(i);
+
+//            every statement must have  a curlybrace{}
+
+
+
+        }
+    }
+}
