680146a6e370d380642b33d4970470269f5be72b
Ero Terry <eroterry24@gmail.com>
2023-11-03 23:43:54 +0100

    add more attribute

--- IdeaProjects/java-programming/src/Classwork.java
+++ IdeaProjects/java-programming/src/Classwork.java
@@ -1,21 +1,9 @@
 public class Classwork {
 
     public static void main(String[] args) {
-//
-//        Created an Array variable and name it "sum that store lists of integers
-//          To add a value inside an array, you need "{add value here}"
 
-        int[] sum = {2, 5, 8, 1, 0, 4, 78,34,21};
-
-//        Tell the for loop to start from the beginning of the array, then tell it to stop at the
-//        length of the array and of course, tell it to kickstart the counting
-        for (int i = 0; i <= sum.length;i ++){
-        System.out.println("This is an array lenght" +i);{
-                {
-
-                }
-            }
-            }
+        int[] number ={30000000, 43, 12, 10};
+        System.out.println(number[2]);
 
 
     }
