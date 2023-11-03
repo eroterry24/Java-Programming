a164764f554a4079bc82e66afbb2baf5e2df1ea1
Ero Terry <eroterry24@gmail.com>
2023-11-03 20:55:08 +0100

    add more attribute

---
+++ IdeaProjects/java-programming/src/ForLoopClassWork.java
@@ -0,0 +1,14 @@
+public class ForLoopClassWork {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i<= 20; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+
+//                Write a Java program to print all even number between 1 and 20 using ForLooop
+            }
+
+        }
+    }
+}
