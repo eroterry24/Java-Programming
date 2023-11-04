4a70cc2040ed2c617f13c13db9b11944e10338ac
Ero Terry <eroterry24@gmail.com>
2023-11-04 13:58:08 +0100

    add more attribute

---
+++ IdeaProjects/java-programming/src/LargeAndSmallNumba.java
@@ -0,0 +1,28 @@
+public class LargeAndSmallNumba {
+
+    public static void main(String[] args) {
+//     Create an array of numbers
+
+       int [] numberArray = {3, 4, 21, 98, 4, 1};
+
+
+       int smallestNumber = numberArray[0];// Assume the first element is the minimum
+       int largestNumber = numberArray[0];// Assume the first element is the maximum
+
+//       then i looped over the array. Giving it the starting point and the ending point
+       for (int index = 1; index < numberArray.length; index++){
+           if (numberArray[index] < smallestNumber) {
+               smallestNumber = numberArray[index];
+           }
+           if (numberArray[index] > largestNumber){
+               largestNumber = numberArray[index];
+
+
+           }
+       }
+        System.out.println("The largest number is" + largestNumber);
+        System.out.println("The smallest number is " + smallestNumber);
+    }
+
+
+}
